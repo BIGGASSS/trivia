46170de3e6d4fcc4426dfad78dff473f903a70ec
BIGGASSS <timcooksgrandpa@gmail.com>
2026-06-09 23:09:49 +0800

    Remove Vue devtools

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
 import { multiplayerPlugin } from "./server/multiplayer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), multiplayerPlugin()],
+  plugins: [vue(), multiplayerPlugin()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
